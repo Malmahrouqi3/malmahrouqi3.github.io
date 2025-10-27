523d238c88ea571ad09e8668c46d7547f7933d2f
Malmahrouqi3 <mohdsaid497566@gmail.com>
2025-10-27 03:07:35 -0400

    trying things

---
+++ documentation/data.js
@@ -0,0 +1,108 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1761546795542,
+  "repoUrl": "https://github.com/Malmahrouqi3/MFC-mo2",
+  "entries": {
+    "Continuous Benchmarking": [
+      {
+        "commit": {
+          "author": {
+            "name": "Malmahrouqi3",
+            "email": "mohdsaid497566@gmail.com"
+          },
+          "committer": {
+            "name": "Malmahrouqi3",
+            "email": "mohdsaid497566@gmail.com"
+          },
+          "id": "8c7d83b533a052dbcf598b54312b1a1bfae99a01",
+          "message": "removed ref",
+          "timestamp": "2025-10-27T06:27:09Z",
+          "url": "https://github.com/Malmahrouqi3/MFC-mo2/commit/8c7d83b533a052dbcf598b54312b1a1bfae99a01"
+        },
+        "date": 1761546495790,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "5eq_rk3_weno3_hllc",
+            "value": 29275,
+            "unit": "undefined/iter",
+            "extra": "iterations: 94877\ncpu: 29836 undefined\nthreads: undefined"
+          },
+          {
+            "name": "hypo_hll",
+            "value": 32317,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21609\ncpu: 32429 undefined\nthreads: undefined"
+          },
+          {
+            "name": "ibm",
+            "value": 32317,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21609\ncpu: 32429 undefined\nthreads: undefined"
+          },
+          {
+            "name": "igr",
+            "value": 32317,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21609\ncpu: 32429 undefined\nthreads: undefined"
+          },
+          {
+            "name": "viscous_weno5_sgb_acoustic",
+            "value": 32724,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21393\ncpu: 33355 undefined\nthreads: undefined"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Malmahrouqi3",
+            "email": "mohdsaid497566@gmail.com"
+          },
+          "committer": {
+            "name": "Malmahrouqi3",
+            "email": "mohdsaid497566@gmail.com"
+          },
+          "id": "8c7d83b533a052dbcf598b54312b1a1bfae99a01",
+          "message": "removed ref",
+          "timestamp": "2025-10-27T06:27:09Z",
+          "url": "https://github.com/Malmahrouqi3/MFC-mo2/commit/8c7d83b533a052dbcf598b54312b1a1bfae99a01"
+        },
+        "date": 1761546795084,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "5eq_rk3_weno3_hllc",
+            "value": 29275,
+            "unit": "undefined/iter",
+            "extra": "iterations: 94877\ncpu: 29836 undefined\nthreads: undefined"
+          },
+          {
+            "name": "hypo_hll",
+            "value": 32317,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21609\ncpu: 32429 undefined\nthreads: undefined"
+          },
+          {
+            "name": "ibm",
+            "value": 32317,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21609\ncpu: 32429 undefined\nthreads: undefined"
+          },
+          {
+            "name": "igr",
+            "value": 32317,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21609\ncpu: 32429 undefined\nthreads: undefined"
+          },
+          {
+            "name": "viscous_weno5_sgb_acoustic",
+            "value": 32724,
+            "unit": "undefined/iter",
+            "extra": "iterations: 21393\ncpu: 33355 undefined\nthreads: undefined"
+          }
+        ]
+      }
+    ]
+  }
+}
