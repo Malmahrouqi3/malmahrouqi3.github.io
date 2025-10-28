11fe124733554fffa5d57493f81564196ed62dc6
github-action-benchmark <github@users.noreply.github.com>
2025-10-28 21:07:04 +0000

    add Continuous Benchmarking (googlecpp) benchmark result for 996a0a12a3e131735fdfb4ad085d1e87cf69b8f0

--- documentation/bench/data.js
+++ documentation/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761682019607,
+  "lastUpdate": 1761685624408,
   "repoUrl": "https://github.com/Malmahrouqi3/MFC-mo2",
   "entries": {
     "Continuous Benchmarking": [
@@ -152,6 +152,58 @@ window.BENCHMARK_DATA = {
             "value": 32724,
             "unit": "s/iter",
             "extra": "iterations: 21393\ncpu: undefined s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Mohammed S. Al-Mahrouqi",
+            "username": "Malmahrouqi3",
+            "email": "145478595+Malmahrouqi3@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "996a0a12a3e131735fdfb4ad085d1e87cf69b8f0",
+          "message": "Standardize thread count to 1 in report.json",
+          "timestamp": "2025-10-28T21:04:17Z",
+          "url": "https://github.com/Malmahrouqi3/MFC-mo2/commit/996a0a12a3e131735fdfb4ad085d1e87cf69b8f0"
+        },
+        "date": 1761685623038,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "5eq_rk3_weno3_hllc",
+            "value": 29275,
+            "unit": "s/iter",
+            "extra": "iterations: 94877\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "hypo_hll",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "ibm",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "igr",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "viscous_weno5_sgb_acoustic",
+            "value": 32724,
+            "unit": "s/iter",
+            "extra": "iterations: 21393\ncpu: 29275 s\nthreads: 1"
           }
         ]
       }
