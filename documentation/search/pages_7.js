828e6564929bca8c3796a6893fb4a457dc3883bb
MFC Action <>
2025-11-03 06:40:00 +0000

    Docs @ c074f41

--- documentation/search/pages_7.js
+++ documentation/search/pages_7.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['papers_0',['Papers',['../md_papers.html',1,'']]],
   ['parallelization_1',['GPU Parallelization',['../md_gpuParallelization.html',1,'']]],
-  ['performance_2',['Performance',['../md_expectedPerformance.html',1,'']]]
+  ['performance_2',['Performance',['../expectedPerformance.html',1,'Expected Performance'],['../Performance.html',1,'Performance']]]
 ];
