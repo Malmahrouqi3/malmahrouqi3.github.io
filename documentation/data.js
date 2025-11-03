f1b8f128623c4b82512274ed6f620277212f9439
github-action-benchmark <github@users.noreply.github.com>
2025-11-03 06:19:23 +0000

    add GT Phoenix (GPU) (googlecpp) benchmark result for df12c2d53c69a2c3f725e3fbfbc14638e1e1ed60

--- documentation/data.js
+++ documentation/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762150761020,
+  "lastUpdate": 1762150763392,
   "repoUrl": "https://github.com/Malmahrouqi3/MFC-mo2",
   "entries": {
     "GT Phoenix (CPU)": [
@@ -21,6 +21,60 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Malmahrouqi3/MFC-mo2/commit/df12c2d53c69a2c3f725e3fbfbc14638e1e1ed60"
         },
         "date": 1762150759446,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "5eq_rk3_weno3_hllc",
+            "value": 29275,
+            "unit": "s/iter",
+            "extra": "iterations: 94877\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "hypo_hll",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "ibm",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "igr",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "viscous_weno5_sgb_acoustic",
+            "value": 32724,
+            "unit": "s/iter",
+            "extra": "iterations: 21393\ncpu: 29275 s\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "GT Phoenix (GPU)": [
+      {
+        "commit": {
+          "author": {
+            "name": "Mohammed S. Al-Mahrouqi",
+            "username": "Malmahrouqi3",
+            "email": "145478595+Malmahrouqi3@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "df12c2d53c69a2c3f725e3fbfbc14638e1e1ed60",
+          "message": "Rename benchmark names and update data directory",
+          "timestamp": "2025-11-03T05:54:25Z",
+          "url": "https://github.com/Malmahrouqi3/MFC-mo2/commit/df12c2d53c69a2c3f725e3fbfbc14638e1e1ed60"
+        },
+        "date": 1762150761897,
         "tool": "googlecpp",
         "benches": [
           {
