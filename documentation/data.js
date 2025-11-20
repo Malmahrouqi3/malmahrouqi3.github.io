2d7fe19943e5791d6fa5c41b064287bebee64cce
github-action-benchmark <github@users.noreply.github.com>
2025-11-20 18:05:57 +0000

    add GT Phoenix (CPU) (googlecpp) benchmark result for 2d21a9623bbf682715a6e6416682f589a1a34324

--- documentation/data.js
+++ documentation/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763661951539,
+  "lastUpdate": 1763661957108,
   "repoUrl": "https://github.com/Malmahrouqi3/MFC-mo2",
   "entries": {
     "GT Phoenix (CPU)": [
@@ -156,6 +156,58 @@ window.BENCHMARK_DATA = {
             "value": 32724,
             "unit": "s",
             "extra": "time steps: 21393\nruntime: 29275 s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Mohammed S. Al-Mahrouqi",
+            "username": "Malmahrouqi3",
+            "email": "145478595+Malmahrouqi3@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "2d21a9623bbf682715a6e6416682f589a1a34324",
+          "message": "Remove Linkcheck step from docs workflow\n\nRemoved the Linkcheck step using Lychee from the workflow.",
+          "timestamp": "2025-11-20T17:55:09Z",
+          "url": "https://github.com/Malmahrouqi3/MFC-mo2/commit/2d21a9623bbf682715a6e6416682f589a1a34324"
+        },
+        "date": 1763661952150,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "5eq_rk3_weno3_hllc",
+            "value": 29275,
+            "unit": "s/iter",
+            "extra": "iterations: 94877\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "hypo_hll",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "ibm",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "igr",
+            "value": 32317,
+            "unit": "s/iter",
+            "extra": "iterations: 21609\ncpu: 29275 s\nthreads: 1"
+          },
+          {
+            "name": "viscous_weno5_sgb_acoustic",
+            "value": 32724,
+            "unit": "s/iter",
+            "extra": "iterations: 21393\ncpu: 29275 s\nthreads: 1"
           }
         ]
       }
